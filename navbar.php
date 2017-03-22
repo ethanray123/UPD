afb7626ee14bf68661c8753e769db1f41d195cf4
ethanray123 <ethanray19@gmail.com>
2017-03-22 17:25:19 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,52 @@
+<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
+        <div class="container-fluid">
+            <div>
+                <ul class="nav navbar-nav">
+                  <li class=""><a href="viewDrugs.php">View Drugs</a></li>
+                  <li></li>
+                </ul>
+                <div class="pull-right nav navbar-nav">
+                    <?php 
+                        if(isset($_SESSION['name'])){ 
+                            echo "<a class='btn btn-md btn-outline' href='logout.php'>
+                                    <span class='glyphicon glyphicon-off' aria-hidden='true' aria-label='Log-out'></span>
+                                </a>";
+                        }else{
+                            echo "<form class='form-inline' method='post' action='login_check.php'> 
+                                    <input type='email' name='email' class='form-control' placeholder='email'>
+                                    <input type='password' name='password'  class='form-control' placeholder='Password'>
+                                    <button class='btn btn-success'>Login</button>
+                                </form>";
+                        }
+                    ?>
+                </div>
+            </div>
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <!-- <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand pull-center" href="#page-top">Up Pharma Down</a>
+            </div> -->
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <!-- <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="hidden">
+                        <a href="#page-top"></a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#about">About</a>
+                    </li>
+                    <li class="page-scroll">
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div> -->
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
